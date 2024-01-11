ab659f08fede3e9cefef4cf283f93c4e6f7f4be5
Asif Anam <anam1828@cseku.ac.bd>
2024-01-11 16:39:02 +0600

    video 251 done

--- src/main/java/com/asifanam/demosecurity/controller/DemoController.java
+++ src/main/java/com/asifanam/demosecurity/controller/DemoController.java
@@ -9,4 +9,11 @@ public class DemoController {
     public String showHome(){
         return "home";
     }
+
+    //add request mapping for /leaders
+    @GetMapping("/leaders")
+    public String showLeaders(){
+        return "leaders";
+    }
+
 }
